3d2481140841e6baf978404cd9d39175f8dfe0f4
toto <k023g0060@m.neec.ac.jp>
2026-02-07 00:23:02 +0900

    WinAppの実装

---
+++ Platform/win/pch.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからあまり使われない部分を除外する
+#define NOMINMAX                        // min と max マクロの定義を防止する
+#include <Windows.h>
--- Platform/win/win_platform.cpp
+++ Platform/win/win_platform.cpp
@@ -1,3 +1,4 @@
+#include "pch.h"
 #include "win_platform.h"
 
 namespace Cue
