92c354447bfea6af04a4f2db546cec0c549122fb
tochouseito <146930194+tochouseito@users.noreply.github.com>
2026-03-09 11:26:51 +0900

    AssetManager追加

---
+++ projects/Engine/asset/AssetManager.cpp
@@ -0,0 +1,6 @@
+#include "AssetManager.h"
+
+namespace Cue::Asset
+{
+    // 1) 現状は空の実装。将来的に、アセットのロード/管理機能をここに実装する予定。
+}
---
+++ projects/Engine/asset/AssetManager.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Cue::Asset
+{
+    class AssetManager final
+    {
+        public:
+        AssetManager() = default;
+        ~AssetManager() = default;
+    };
+}
