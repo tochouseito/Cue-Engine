f99efaf0b7b6303c4c74b8b1bbbbb3e6980ba4d9
tochouseito <146930194+tochouseito@users.noreply.github.com>
2026-03-16 11:07:39 +0900

    Editorのコメント修正

--- projects/Editor/Editor.cpp
+++ projects/Editor/Editor.cpp
@@ -35,17 +35,22 @@
 // windows アプリのエントリーポイント
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-    // 1) プラットフォームとグラフィックスバックエンドの作成
+    // 1) 各パラメータ
+    uint32_t screenWidth = 1280;
+    uint32_t screenHeight = 720;
+    uint32_t bufferCount = 3;
+
+    // 2) プラットフォームとグラフィックスバックエンドの作成
     auto win = std::make_unique<Cue::Platform::Win::WinPlatform>();
     auto d3d12Backend = std::make_unique<Cue::GraphicsCore::DX12::D3D12Backend>();
 
-    // 2) win platform を d3d12 backend へ設定
+    // 3) win platform を d3d12 backend へ設定
     d3d12Backend->set_win_platform(win.get());
 
-    // 3) win platform 初期化
+    // 4) win platform 初期化
     win->setup();
 
-    // 4) d3d12 backend 初期化
+    // 5) d3d12 backend 初期化
     Cue::GraphicsCore::backend_setup_info backendSetupInfo{};
     backendSetupInfo.bufferCount = 3;
     backendSetupInfo.screenWidth = win->window_width();
@@ -53,9 +58,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     backendSetupInfo.transformBufferPoolDesc.slotCapacity = 2;
     d3d12Backend->initialize(backendSetupInfo);
 
-    // 4) editor 初期化
+    // 6) editor 初期化
 
-    // 4-1) ImGuiManager 初期化設定を構築
+    // 6-1) ImGuiManager 初期化設定を構築
     Cue::Editor::imgui_setup_info imguiSetupInfo;
     imguiSetupInfo.hwnd = static_cast<HWND>(win->get_native_window_handle());   // ImGui_ImplWin32_WndProcHandler へ渡すために HWND にキャスト
     imguiSetupInfo.device = d3d12Backend->get_device();                         // ImGui_ImplDX12_Init へ渡すために ID3D12Device* を取得
@@ -66,11 +71,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     imguiSetupInfo.fontSrvCpuDescHandle = fontSrvInfo.cpuDescHandle;            // ImGui_ImplDX12_Init へ渡すために フォント用 SRV の CPU デスクリプタハンドルを取得
     imguiSetupInfo.fontSrvGpuDescHandle = fontSrvInfo.gpuDescHandle;            // ImGui_ImplDX12_Init へ渡すために フォント用 SRV の GPU デスクリプタハンドルを取得
 
-    // 4-2) ImGuiManager を初期化
+    // 6-2) ImGuiManager を初期化
     Cue::Editor::ImGuiManager imguiManager;
     imguiManager.initialize(imguiSetupInfo);
 
-    // 4-3) ImGuiMessageHandler を登録
+    // 6-3) ImGuiMessageHandler を登録
     const uint64_t imguiMessageHandlerId = win->register_message_handler(
         [](Cue::Platform::Win::NativeWindowHandle hwnd, uint32_t msg, std::uintptr_t wParam, std::intptr_t lParam, std::intptr_t& outResult)
         {
@@ -89,33 +94,33 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         });
 
 
-    // 5) エンジンの初期化
+    // 7) エンジンの初期化
 
-    // 5-1) 初期化情報構築
+    // 7-1) 初期化情報構築
     Cue::EngineInitInfo initInfo;
     initInfo.platform = win.get();
     initInfo.graphicsBackend = d3d12Backend.get();
 
-    // 5-2) editor pass として ImGuiPass を登録
+    // 7-2) editor pass として ImGuiPass を登録
     std::unique_ptr<Cue::Editor::ImGuiPass> imguiPass = std::make_unique<Cue::Editor::ImGuiPass>(imguiManager);
     initInfo.editorPass = std::move(imguiPass);
 
     Cue::Engine engine;
     bool isRunning = engine.initialize(initInfo);
 
-    // 5-3) debug view 作成
+    // 7-3) debug view 作成
     Cue::Editor::DebugView debugView(*win, engine);
 
-    // 5-4) statistics 作成
+    // 7-4) statistics 作成
     Cue::Editor::Statistics statistics(engine.frame_controller());
 
-    // 6) メインループ
+    // 8) メインループ
     while (isRunning)
     {
-        // 6-1) Windows のメッセージ処理
+        // 8-1) Windows のメッセージ処理
         isRunning = win->poll_message();
 
-        // 6-2) imgui フレーム開始
+        // 8-2) imgui フレーム開始
         if (imguiManager.begin_frame())
         {
             ImGui::Begin("Hello, ImGui!"); // ウィンドウ開始
@@ -128,7 +133,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
             imguiManager.end_frame();
         }
 
-        // 5-2) エンジンの更新と描画
+        // 8-2) エンジンの更新と描画
         engine.tick();
     }
 
@@ -137,18 +142,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         (void)win->unregister_message_handler(imguiMessageHandlerId);
     }
 
-    // 6) editor シャットダウン
+    // 9) editor シャットダウン
     imguiManager.shutdown();
 
-    // 7) エンジンのシャットダウン
+    // 10) エンジンのシャットダウン
     engine.shutdown();
 
-    // 8) グラフィックスバックエンドのシャットダウン
+    // 11) グラフィックスバックエンドのシャットダウン
     d3d12Backend->shutdown();
 
-    // 9) プラットフォームのシャットダウン
+    // 12) プラットフォームのシャットダウン
     win->shutdown();
 
-    // 10) アプリケーションの終了
+    // 13) アプリケーションの終了
     return 0;
 }
